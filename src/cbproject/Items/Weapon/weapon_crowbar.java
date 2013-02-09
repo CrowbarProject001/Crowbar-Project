f1f0d10b0e363db4b461f2b9745f56607dc8ad17
mkpoli <snowwolf1001@gmail.com>
2013-02-09 21:49:48 +0800

    233

--- src/cbproject/Items/Weapon/weapon_crowbar.java
+++ src/cbproject/Items/Weapon/weapon_crowbar.java
@@ -1,13 +1,13 @@
-package cbproject.Items;
+package cbproject.Items.Weapon;
 
 import cbproject.Proxy.ClientProxy;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.ItemSword;
 
-public class Crowbar extends ItemSword {
+public class weapon_crowbar extends ItemSword {
 
-	public Crowbar(int item_id) {
+	public weapon_crowbar(int item_id) {
 		super(item_id, EnumToolMaterial.IRON);
 		
 		setItemName("Crowbar");
