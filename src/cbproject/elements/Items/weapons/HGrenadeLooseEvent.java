ecdd36116c3ac0f8c92bd9faeb33cb7e2e9c5b03
WeAthFolD <Cheetah77@qq.com>
2013-02-15 23:24:22 +0800

    Added grenade prototype
    233 done for today

---
+++ src/cbproject/elements/Items/weapons/HGrenadeLooseEvent.java
@@ -0,0 +1,17 @@
+package cbproject.elements.Items.weapons;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+public class HGrenadeLooseEvent extends PlayerEvent {
+	public final ItemStack itemGren;
+	public int charge;
+	public HGrenadeLooseEvent(EntityPlayer player, ItemStack gren, int chg) {
+		super(player);
+		itemGren = gren;
+		this.charge=chg;
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/cbproject/elements/Items/weapons/Weapon_hgrenade.java
+++ src/cbproject/elements/Items/weapons/Weapon_hgrenade.java
@@ -2,7 +2,12 @@ package cbproject.elements.Items.weapons;
 
 import cbproject.proxy.ClientProxy;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ArrowNockEvent;
 
 public class Weapon_hgrenade extends Item {
 
@@ -15,5 +20,28 @@ public class Weapon_hgrenade extends Item {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/*
+	 * 放开使用键时的行为
+	 * @see net.minecraft.item.Item#onPlayerStoppedUsing(net.minecraft.item.ItemStack, net.minecraft.world.World, net.minecraft.entity.player.EntityPlayer, int)
+	 */
+	@Override
+    public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4)
+    {
+		return;
+    }
 	
+	/*
+	 * 右键点击的行为
+	 * @see net.minecraft.item.Item#onItemRightClick(net.minecraft.item.ItemStack, net.minecraft.world.World, net.minecraft.entity.player.EntityPlayer)
+	 */
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        // ArrowNockEvent event = new ArrowNockEvent(par3EntityPlayer, par1ItemStack);
+        //MinecraftForge.EVENT_BUS.post(event);
+
+		
+        par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+        return par1ItemStack;
+    }
 }
