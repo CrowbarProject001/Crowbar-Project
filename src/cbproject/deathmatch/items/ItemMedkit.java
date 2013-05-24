d56c0c0e485dcf8b918dc480d0536f8f649bc932
mkpoli <snowwolf1001@gmail.com>
2013-05-24 21:38:12 +0800

    changed medkit item

--- src/cbproject/deathmatch/items/ItemMedkit.java
+++ src/cbproject/deathmatch/items/ItemMedkit.java
@@ -14,12 +14,16 @@
  */
 package cbproject.deathmatch.items;
 
+import java.util.List;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import cbproject.api.energy.item.ICustomEnItem;
 import cbproject.core.CBCMod;
@@ -31,6 +35,8 @@ import cbproject.deathmatch.entities.EntityMedkit;
  * @author mkpoli
  */
 public class ItemMedkit extends CBCGenericItem {
+
+	
 	
 	public ItemMedkit(int par1) {
 		super(par1);
@@ -38,24 +44,47 @@ public class ItemMedkit extends CBCGenericItem {
 		setUnlocalizedName("hevbattery");
 		this.setIconName("medkit");
 	}
+	
 
-    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
-    {
-    	
-    	if(!par2World.isRemote){
-    		EntityMedkit ent = new EntityMedkit(par2World);
-    		par2World.spawnEntityInWorld(ent);
-    		par1ItemStack.splitStack(1);
-    	}
-        return par1ItemStack;
-    }
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,
+			EntityPlayer par3EntityPlayer) {
 
+		if (!par2World.isRemote) {
+			EntityMedkit ent = new EntityMedkit(par2World);
+			par2World.spawnEntityInWorld(ent);
+			par1ItemStack.splitStack(1);
+		}
+		return par1ItemStack;
+	}
+
+	
 	@Override
 	public void setIconName(String name) {
-		// TODO Auto-generated method stub
 		super.setIconName(name);
 	}
-    
-    
-    
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack par1ItemStack,
+			EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+		par1ItemStack.stackTagCompound.setString("potion1name", "potion1.name");
+		par1ItemStack.stackTagCompound.setString("potion1name", "potion1.name");
+		par1ItemStack.stackTagCompound.setString("potion1name", "potion1.name");
+		par3List.add(StatCollector.translateToLocal("potion.name")
+				+ " : "
+				+ (par1ItemStack.getMaxDamage() - par1ItemStack
+						.getItemDamageForDisplay()) + "/"
+				+ par1ItemStack.getMaxDamage() + " EU");
+	}
+	
+	protected int getItemCharge(ItemStack stack){
+		return loadCompound(stack).getInteger("charge");
+	}
+	
+	private NBTTagCompound loadCompound(ItemStack stack){
+		if(stack.stackTagCompound == null)
+			stack.stackTagCompound = new NBTTagCompound();
+		return stack.stackTagCompound;
+	}
+	
 }
